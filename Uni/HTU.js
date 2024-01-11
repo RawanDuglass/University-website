3c42d7b2d94cc052fbebee632921cbd8ba13c390
Rawan <rawan_duglass@yahoo.com>
2024-01-11 19:19:24 +0300

    initial commit

---
+++ Uni/HTU.js
@@ -0,0 +1,10 @@
+<script>
+ function sum() 
+{ 
+    
+    var n1 = document.getElementByTa; 
+    var n2 = document.getElementById("n2").value; 
+    result =  (fn+ln); 
+    document.getElementById("demo2").innerHTML = result; 
+}
+</script>
